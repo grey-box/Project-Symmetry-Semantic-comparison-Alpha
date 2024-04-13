7273a65f93178fa5d7cba4a66766ce73f9f322a8
elfarsif <f.n.elfarsi@gmail.com>
2024-04-13 13:33:24 -0400

    uncommented nltk translator for ci test

--- api/flaskr/services/articleService.py
+++ api/flaskr/services/articleService.py
@@ -14,19 +14,18 @@ def get_article(url):
         article=scrape.getArticle(url)
     except Exception as e:
         return make_response({"Error getting original article":str(e)},404)
-    
     # 2.get the english version of that article
-
     try:
         article['secondLanguage']=scrape.getArticle("https://en.wikipedia.org/wiki/Water_scarcity")
+
     except Exception as e: 
         return make_response({"Error getting 2nd language article":str(e)},404)
 
     try:
-        translatedArticle=translation.translate("fr",article['secondLanguage']['text'],"Google translate","")
-        article['translatedSecondLanguage']=translatedArticle
-        #colorsGenerated = colors.gen_colors()
-        #article['comparisonInfo']=bleuScore.compare(article['text'],article['translatedSecondLanguage'],colorsGenerated,0.1)
+        #translatedArticle=translation.translate("fr",article['secondLanguage']['text'],"Google translate","")
+        #article['translatedSecondLanguage']=translatedArticle
+        colorsGenerated = colors.gen_colors()
+        article['comparisonInfo']=bleuScore.compare('this is one article','this is another article',colorsGenerated,0.1)
         return article
     except Exception as e:
         return make_response({"Error translating":str(e)},404)
