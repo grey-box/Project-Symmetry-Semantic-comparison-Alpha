08ce1aed0a870b5aff9c8280e0be328d216def86
elfarsif <f.n.elfarsi@gmail.com>
2024-04-13 13:17:32 -0400

    Testing pipeline test functions
    - Commented out all articleService functionalities other than scrape

--- api/flaskr/services/articleService.py
+++ api/flaskr/services/articleService.py
@@ -12,20 +12,22 @@ def get_article(url):
     # 1.get the french article
     try:
         article=scrape.getArticle(url)
-    except Exception as e:
-        return make_response({"Error getting original article":str(e)},404)
-    # 2.get the english version of that article
-    try:
-        article['secondLanguage']=scrape.getArticle("https://en.wikipedia.org/wiki/Water_scarcity")
-
-    except Exception as e: 
-        return make_response({"Error getting 2nd language article":str(e)},404)
-
-    try:
-        translatedArticle=translation.translate("fr",article['secondLanguage']['text'],"Google translate","")
-        article['translatedSecondLanguage']=translatedArticle
-        colorsGenerated = colors.gen_colors()
-        article['comparisonInfo']=bleuScore.compare(article['text'],article['translatedSecondLanguage'],colorsGenerated,0.1)
         return article
     except Exception as e:
-        return make_response({"Error translating":str(e)},404)
+        return make_response({"Error getting original article":str(e)},404)
+    
+    # 2.get the english version of that article
+    # try:
+    #     article['secondLanguage']=scrape.getArticle("https://en.wikipedia.org/wiki/Water_scarcity")
+
+    # except Exception as e: 
+    #     return make_response({"Error getting 2nd language article":str(e)},404)
+
+    # try:
+    #     translatedArticle=translation.translate("fr",article['secondLanguage']['text'],"Google translate","")
+    #     article['translatedSecondLanguage']=translatedArticle
+    #     colorsGenerated = colors.gen_colors()
+    #     article['comparisonInfo']=bleuScore.compare(article['text'],article['translatedSecondLanguage'],colorsGenerated,0.1)
+    #     return article
+    # except Exception as e:
+    #     return make_response({"Error translating":str(e)},404)
