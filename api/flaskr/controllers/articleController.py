d481e01a0c33affe2759fcc2809936fc74a95b09
frankfarsi <166565875+frankfarsi@users.noreply.github.com>
2024-04-23 13:08:41 -0400

    Add api structure (#22)
    * Change for python binary in linux
    
    * added target language route/service/model
    
    * changed version number in package
    
    ---------
    
    Co-authored-by: elfarsif <f.n.elfarsi@gmail.com>

--- api/flaskr/controllers/articleController.py
+++ api/flaskr/controllers/articleController.py
@@ -3,15 +3,17 @@ from flaskr.services import articleService
 import json
 from flaskr.models.articleModel import ArticleModel
 
-article_route = Blueprint('product_route', __name__,url_prefix="/test")
+article_route = Blueprint("product_route", __name__, url_prefix="/test")
 
-@article_route.route("",methods=['POST'])
+
+@article_route.route("", methods=["POST"])
 def get_article():
-    data=request.get_json()
-    url=data.get('url')
+    data = request.get_json()
+    url = data.get("url")
     return articleService.get_article(url)
 
-@article_route.route("/originalArticle",methods=['POST'])
+
+@article_route.route("/originalArticle", methods=["POST"])
 def get_original_article():
     """
     Retrieves the original article based on the provided URL.
@@ -19,16 +21,11 @@ def get_original_article():
     Returns:
         The original article.
     """
-    article = ArticleModel(request.get_json().get('url'),None)
+    article = ArticleModel(request.get_json().get("url"), None, None)
     return articleService.get_original_article(article)
 
 
-
-
-
-
-
-
-
-
-     
+@article_route.route("/targetArticle", methods=["POST"])
+def get_target_article():
+    article = ArticleModel(request.get_json().get("url"), None, None)
+    return articleService.fetch_target_article(article, "en")
--- api/flaskr/services/articleService.py
+++ api/flaskr/services/articleService.py
@@ -1,40 +1,49 @@
 from flask import make_response
-from flaskr.scripts import scrape,translation,bleuScore,colors
+from flaskr.scripts import scrape, translation, bleuScore, colors
 from flaskr.models.articleModel import ArticleModel
 import json
 
-def get_original_article(article:ArticleModel):
+
+def get_original_article(article: ArticleModel):
     try:
         article.set_original_article(scrape.getArticle(article.get_article_url()))
         return article.get_original_article()
     except Exception as e:
-        return make_response({"Error getting original article":str(e)},404)
+        return make_response({"Error getting original article": str(e)}, 404)
 
+
+def fetch_target_article(article: ArticleModel, targetLanguage: str):
+    return {"message": "fetch target article"}
 
 
 def get_article(url):
-
-    
     # 3.translate the english article to french
     # 4.compare the differences between the original french article and english translated to french article
-    
+
     # 1.get the french article
     try:
-        article=scrape.getArticle(url)
+        article = scrape.getArticle(url)
     except Exception as e:
-        return make_response({"Error getting original article":str(e)},404)
+        return make_response({"Error getting original article": str(e)}, 404)
     # 2.get the english version of that article
     try:
-        article['secondLanguage']=scrape.getArticle("https://en.wikipedia.org/wiki/Water_scarcity")
+        article["secondLanguage"] = scrape.getArticle(
+            "https://en.wikipedia.org/wiki/Water_scarcity"
+        )
 
-    except Exception as e: 
-        return make_response({"Error getting 2nd language article":str(e)},404)
+    except Exception as e:
+        return make_response({"Error getting 2nd language article": str(e)}, 404)
 
     try:
-        translatedArticle=translation.translate("fr",article['secondLanguage']['text'],"Google translate","")
-        article['translatedSecondLanguage']=translatedArticle
+        translatedArticle = translation.translate(
+            "fr", article["secondLanguage"]["text"], "Google translate", ""
+        )
+        article["translatedSecondLanguage"] = translatedArticle
         colorsGenerated = colors.gen_colors()
-        article['comparisonInfo']=bleuScore.compare('this is one article','this is another article',colorsGenerated,0.1)
+        article["comparisonInfo"] = bleuScore.compare(
+            "this is one article", "this is another article", colorsGenerated, 0.1
+        )
         return article
     except Exception as e:
-        return make_response({"Error translating":str(e)},404)
+        return make_response({"Error translating": str(e)}, 404)
+
