4bb944fed267a93ad35fb7bd81133d2ffef3ea3e
elfarsif <f.n.elfarsi@gmail.com>
2024-04-23 13:01:32 -0400

    added target language route/service/model

--- api/flaskr/models/articleModel.py
+++ api/flaskr/models/articleModel.py
@@ -1,19 +1,28 @@
 class ArticleModel:
-    def __init__(self, articleUrl, originalArticle):
-        self.articleUrl=articleUrl
-        self.originalArticle=originalArticle
+    def __init__(self, articleUrl, originalArticle, targetArticle):
+        self.articleUrl = articleUrl
+        self.originalArticle = originalArticle
+        self.targetArticle = targetArticle
 
     def get_article_url(self):
         return self.articleUrl
 
     def set_article_url(self, articleUrl):
         self.articleUrl = articleUrl
-    
+
     def get_original_article(self):
         return self.originalArticle
 
     def set_original_article(self, originalArticle):
         self.originalArticle = originalArticle
-    
+
+    def get_target_article(self):
+        return self.targetArticle
+
+    def set_target_article(self, targetArticle):
+        self.targetArticle = targetArticle
+
     def __str__(self):
-        return f"Article URL: {self.articleUrl}\nOriginal Article: {self.originalArticle}"
+        return (
+            f"Article URL: {self.articleUrl}\nOriginal Article: {self.originalArticle}"
+        )
