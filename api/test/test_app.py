d33c129f0847883d6d59dbbda8b3f2141926e5f5
frankfarsi <166565875+frankfarsi@users.noreply.github.com>
2024-04-27 21:45:11 -0400

    Frank clean (#29)
    * cleaner code
    
    -added a function to initialize request into model
    
    * added test case
    
    ---------
    
    Co-authored-by: elfarsif <f.n.elfarsi@gmail.com>

--- api/test/test_app.py
+++ api/test/test_app.py
@@ -20,13 +20,25 @@ def test_get_article(client):
     assert response.is_json
 
 
-def test_translate_controller_translate_article(client):
+def test_translate_controller_source_article(client):
     data = {
         "sourceArticleUrl": "https://en.wikipedia.org/wiki/Water_scarcity",
         "targetLanguage": "https://fr.wikipedia.org/wiki/Stress_hydrique_(%C3%A9cologie)",
         "translationTool": "google",
         "deepLApiKey": "apiKey",
     }
-    response = client.post("/translate/sourceAndTarget", json=data)
+    response = client.post("/translate/sourceArticle", json=data)
+    assert response.status_code == 200
+    assert response.is_json
+
+
+def test_translate_controller_settings_check(client):
+    data = {
+        "sourceArticleUrl": "https://en.wikipedia.org/wiki/Water_scarcity",
+        "targetLanguage": "https://fr.wikipedia.org/wiki/Stress_hydrique_(%C3%A9cologie)",
+        "translationTool": "google",
+        "deepLApiKey": "apiKey",
+    }
+    response = client.post("/translate/settingsCheck", json=data)
     assert response.status_code == 200
     assert response.is_json
