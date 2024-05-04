b4e104439e1902ad690b127307bbdbc18a5bbaf4
frankfarsi <166565875+frankfarsi@users.noreply.github.com>
2024-05-04 15:33:03 -0400

    Tailwind (#32)
    * added a packaging electron-forge-vite-tailwind-app
    
    * removed old author
    
    * removed ui dir,next dir, changed api app name to pyapp
    
    * changed webflow for new app name
    
    ---------
    
    Co-authored-by: elfarsif <f.n.elfarsi@gmail.com>

---
+++ electron-forge-vite-tailwind/src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <p class="underline underline-offset-1 text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+  );
+};
+
+export default App;
