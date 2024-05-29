2e8800d9e494b09fbb20fa6ee567b1dace552a94
elfarsif <f.n.elfarsi@gmail.com>
2024-05-29 01:16:27 -0400

    changed file location

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from uvicorn import run
+from typing import Union
 import uvicorn
 
 app = FastAPI()
@@ -7,6 +8,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/comparisonTool/blueScore/")
+def blue_score():
+    return {"title":"Water Scarcity"}
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app,host='127.0.0.1', port=8000)
