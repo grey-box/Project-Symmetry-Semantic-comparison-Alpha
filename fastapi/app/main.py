f547bbdda58bbe7e46c3b23d21b38d71c33bbcdc
elfarsif <f.n.elfarsi@gmail.com>
2024-06-25 15:28:38 -0400

    added url to enpoints

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -9,18 +9,18 @@ from app.scripts import scrape
 app = FastAPI()
 
 class Url(BaseModel):
-    add:str
+    address:str
 @app.post("/api/v1/article/original/")
 def get_orginal_article_by_url(url:Url):
-    return scrape.getArticle("https://en.wikipedia.org/wiki/Water_scarcity")
+    return scrape.getArticle(url.address)
 
 @app.post("/api/v1/article/original/languages")
-def get_list_of_target_languages_with_corresponding_link():
-    return scrape.languageGetter("https://en.wikipedia.org/wiki/Water_scarcity")
+def get_list_of_target_languages_with_corresponding_link(url:Url):
+    return scrape.languageGetter(url.address)
 
 @app.post("/api/v1/article/original/targetLanguageArticle")
-def get_target_language_article_of_the_orginial_article():
-    return scrape.getArticle("https://fr.wikipedia.org/wiki/Stress_hydrique_(%C3%A9cologie) ")
+def get_target_language_article_of_the_orginial_article(url:Url):
+    return scrape.getArticle(url.address);
 
 
 if __name__ == '__main__':
--- fastapi/app/test/test_main.py
+++ fastapi/app/test/test_main.py
@@ -4,15 +4,14 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_fetch_target_article():
-    data = {"url":"testUrl"}
-    response = client.post("/api/v1/article/original/",json = {"hey"})
+def test_fetch_target_article_water_scarcity_is_200():
+    response = client.post("/api/v1/article/original/",json ={ "address":"https://en.wikipedia.org/wiki/water_scarcity"})
     assert response.status_code == 200
 
 def test_get_list_of_target_languages_for_water_scarcity_page():
-    response = client.post("/api/v1/article/original/languages")
+    response = client.post("/api/v1/article/original/languages",json ={ "address":"https://en.wikipedia.org/wiki/water_scarcity"})
     assert response.status_code == 200
 
 def test_get_target_language_article_of_the_orginial_article():
-    response = client.post("/api/v1/article/original/targetLanguageArticle")
+    response = client.post("/api/v1/article/original/targetLanguageArticle",json ={ "address":"https://en.wikipedia.org/wiki/water_scarcity"})
     assert response.status_code == 200
