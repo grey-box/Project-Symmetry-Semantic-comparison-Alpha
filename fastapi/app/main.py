8a963ddf6f2efb3709e3096b5a397edf49c72a8c
cdvandyk-ca <christopher.dyke@grey-box.ca>
2024-11-11 09:48:38 -0700

    get article by url

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -8,6 +8,27 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # def getArticle
+def getArticle(url):
+    try:
+        page = requests.get(url)
+        page.raise_for_status() # for HTTP errors
+        soup = BeautifulSoup(page.content, "html.parser")
+        title = soup.find('title').text.replace(" - Wikipedia", "")
+        text = ""
+        for paragraph in soup.find_all('p'):
+            text += paragraph.text + " " # Adding a space to keep words separate
+        return {"title": title, "text": text}
+    except requests.exceptions.RequestException as e: # error handling
+        print(f"Error getting article: {e}")
+        return None
+
+@app.get("/get_article/")
+def get_article(url: str)
+    article = getArticle(url)
+    if article:
+        return {"article": article}
+    else: # error handling
+        raise HTTPException(status_code=404, detail="Article not found")
 
 class Url(BaseModel):
     address:str
